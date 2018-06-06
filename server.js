d85494adad7742d2420468ca3eed41c618dbddbf
Erica Johnson <ericacjohnson@gmail.com>
2018-06-05 20:33:56 -0700

    ..

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080, function() {
   console.log('server running');
 });
+
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function () {
+    console.info(`App listening on ${this.address().port}`);
+  });
+}
+
+module.exports = app;
