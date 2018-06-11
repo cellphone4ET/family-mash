d0327290afd5d9a1120d224d19145a4a8db004d0
Erica Johnson <ericacjohnson@gmail.com>
2018-06-11 08:42:57 -0700

    ..

--- public/main.js
+++ public/main.js
@@ -34,7 +34,7 @@ function submitLogin() {
     $('#logo').hide();
     insertPhotos();
     document.body.style.backgroundColor = "white";
-    // document.body.style.marginTop = "0px";
+    document.body.style.padding = "0px";
   });
 }
 
@@ -46,12 +46,11 @@ function submitSignUp() {
     $('#logo').hide();
     insertPhotos();
     document.body.style.backgroundColor = "white";
-    // document.body.style.marginTop = "0px";
   });
 }
 
 function clickMenu() {
-  $('.main-nav').on('click', $('#menu-link'), function(event) {
+  $('.main-nav').on('click', $('.menu-link'), function(event) {
     $('.row').hide();
     $('.add-fam-member').show();
     $('.sign-out').show();
@@ -67,7 +66,7 @@ function signOut() {
     $('.sign-out').hide();
     $('#landing-div').show();
     document.body.style.backgroundColor = "#DCD0C0";
-    document.body.style.marginTop = "150px";
+    // document.body.style.marginTop = "150px";
   });
 }
 
@@ -87,16 +86,15 @@ function submitPerson() {
   });
 }
 
-// function returnToMainScreen() {
-//   $('.main-nav2').on('click', $('#mainpage'), function(event) {
-//     console.log('returnToMainScreen ran');
-//     $('.add-fam-member').hide();
-//     $('.sign-out').hide();
-//     insertPhotos();
-//   });
-// }
-
-
+function returnToMainScreen() {
+  $('.main-nav').on('click', $('#mainpage'), function(event) {
+    console.log('returnToMainScreen ran');
+    $('.add-fam-member').hide();
+    $('.sign-out').hide();
+    $('.create-person-form').hide();
+    insertPhotos();
+  });
+}
 
 function moreInfoLink() {
   $('#photo-div').on('click', $('#family-member-info'), function(event) {
@@ -105,72 +103,73 @@ function moreInfoLink() {
   });
 }
 
+// FOR DISPLAY ONLY: this will need to be reformatted to account for data
 
-// for display only: this will need to be reformatted to account for data
 function insertPersonInfo() {
-let html = `<div class="info-bloc">
-    <div class="row">
-      <div class="col-4">
-        <div class="card">
-          <img alt="family-member" class="card-image" src="morticia.jpg" />
-          <div class="card-content">
-            <h3>Morticia Addams</h3>
-            <p>Mother</p>
-            <ul>
-              <li>DO</li>
-              <li>RE</li>
-              <li>MI</li>
-            <ul>
+  let html = `<div class="info-bloc">
+      <div class="row">
+        <div class="col-4">
+          <div class="card">
+            <img alt="family-member" class="card-image" src="morticia.jpg" />
+            <div class="card-content">
+              <h3>Morticia Addams</h3>
+              <p><span class="content-field">Relation</span>: mother</p>
+              <p><span class="content-field">Age</span>: immortal</p>
+              <p><span class="content-field">Birthday</span>: 01/01/1300</p>
+              <p><span class="content-field">Significant Other</span>: Gomes Addams</p>
+              <p><span class="content-field">Anniversary</span>: 02/14/1666</p>
+              <p><span class="content-field">Notes</span>: needs vitamin D</p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>`
-  $('.person-info').html(html);
-}
+      </div>`
+    $('.person-info').html(html);
+  }
 
-// for display only: this will need to be reformatted to account for data
+// FOR DISPLAY ONLY: this will need to be reformatted to account for data
 function insertPhotos() {
-let html = `
-<div class="row">
+  let html = `
+  <div class="row">
 
-  <div class="col-4">
-    <div class="card">
-      <img alt="family-member" class="card-image" src="morticia.jpg" />
-      <div class="card-content">
-        <h3><a href="#family-member-info">Morticia Addams</a></h3>
-        <p>Mother</p>
+    <div class="col-4">
+      <div class="card">
+        <img alt="family-member" class="card-image" src="morticia.jpg" />
+        <div class="card-content">
+          <h3><a href="#family-member-info">Morticia Addams</a></h3>
+          <p>Mother</p>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="col-4">
-    <div class="card">
-      <img class="card-image" src="gomez.jpg" />
-      <div class="card-content">
-        <h3>Gomez Addams</h3>
-        <p>Father</p>
+    <div class="col-4">
+      <div class="card">
+        <img class="card-image" src="gomez.jpg" />
+        <div class="card-content">
+          <h3>Gomez Addams</h3>
+          <p>Father</p>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="col-4">
-    <div class="card">
-      <img class="card-image" src="uncle-fester.jpg" />
-      <div class="card-content">
-        <h3>Uncle Fester</h3>
-        <p>Uncle</p>
+    <div class="col-4">
+      <div class="card">
+        <img class="card-image" src="uncle-fester.jpg" />
+        <div class="card-content">
+          <h3>Uncle Fester</h3>
+          <p>Uncle</p>
+        </div>
       </div>
     </div>
+
   </div>
+  <div class="row">`
 
-</div>
-<div class="row">`
-
-$('#photo-div').html(html);
-}
+  $('#photo-div').html(html);
+  }
 
 
 $(document).ready(function() {
+  returnToMainScreen();
   moreInfoLink();
   submitPerson();
   createPerson();
