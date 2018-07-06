7474db1e273a4b4c600a44d6c8add91e57a103d8
Erica Johnson <ericacjohnson@gmail.com>
2018-07-06 16:52:05 -0700

    ..

--- test/test-auth.js
+++ test/test-auth.js
@@ -21,7 +21,7 @@
 //   const lastName = 'User';
 //
 //   before(function () {
-//     return runServer();
+//     return runServer(TEST_DATABASE_URL);
 //   });
 //
 //   after(function () {
@@ -60,179 +60,179 @@
 //           expect(res).to.have.status(400);
 //         });
 //     });
-//   //   it('Should reject requests with incorrect usernames', function () {
-//   //     return chai
-//   //       .request(app)
-//   //       .post('/api/auth/login')
-//   //       .send({ email: 'wrongemail', password })
-//   //       .then(() =>
-//   //         expect.fail(null, null, 'Request should not succeed')
-//   //       )
-//   //       .catch(err => {
-//   //         if (err instanceof chai.AssertionError) {
-//   //           throw err;
-//   //         }
-//   //
-//   //         const res = err.response;
-//   //         expect(res).to.have.status(401);
-//   //       });
-//   //   });
-//   //   it('Should reject requests with incorrect passwords', function () {
-//   //     return chai
-//   //       .request(app)
-//   //       .post('/api/auth/login')
-//   //       .send({ email, password: 'wrongPassword' })
-//   //       .then(() =>
-//   //         expect.fail(null, null, 'Request should not succeed')
-//   //       )
-//   //       .catch(err => {
-//   //         if (err instanceof chai.AssertionError) {
-//   //           throw err;
-//   //         }
-//   //
-//   //         const res = err.response;
-//   //         expect(res).to.have.status(401);
-//   //       });
-//   //   });
-//   //   it('Should return a valid auth token', function () {
-//   //     return chai
-//   //       .request(app)
-//   //       .post('/api/auth/login')
-//   //       .send({ email, password })
-//   //       .then(res => {
-//   //         expect(res).to.have.status(200);
-//   //         expect(res.body).to.be.an('object');
-//   //         const token = res.body.authToken;
-//   //         expect(token).to.be.a('string');
-//   //         const payload = jwt.verify(token, JWT_SECRET, {
-//   //           algorithm: ['HS256']
-//   //         });
-//   //         expect(payload.user).to.deep.equal({
-//   //           email,
-//   //           firstName,
-//   //           lastName
-//   //         });
-//   //       });
-//   //   });
-//   });
-//
-//   // describe('/api/auth/refresh', function () {
-//   //   it('Should reject requests with no credentials', function () {
-//   //     return chai
-//   //       .request(app)
-//   //       .post('/api/auth/refresh')
-//   //       .then(() =>
-//   //         expect.fail(null, null, 'Request should not succeed')
-//   //       )
-//   //       .catch(err => {
-//   //         if (err instanceof chai.AssertionError) {
-//   //           throw err;
-//   //         }
-//   //
-//   //         const res = err.response;
-//   //         expect(res).to.have.status(401);
-//   //       });
-//   //   });
-//   //   it('Should reject requests with an invalid token', function () {
-//   //     const token = jwt.sign(
-//   //       {
-//   //         email,
-//   //         firstName,
-//   //         lastName
-//   //       },
-//   //       'wrongSecret',
-//   //       {
-//   //         algorithm: 'HS256',
-//   //         expiresIn: '7d'
-//   //       }
-//   //     );
-//   //
-//   //     return chai
-//   //       .request(app)
-//   //       .post('/api/auth/refresh')
-//   //       .set('Authorization', `Bearer ${token}`)
-//   //       .then(() =>
-//   //         expect.fail(null, null, 'Request should not succeed')
-//   //       )
-//   //       .catch(err => {
-//   //         if (err instanceof chai.AssertionError) {
-//   //           throw err;
-//   //         }
-//   //
-//   //         const res = err.response;
-//   //         expect(res).to.have.status(401);
-//   //       });
-//   //   });
-//   //   it('Should reject requests with an expired token', function () {
-//   //     const token = jwt.sign(
-//   //       {
-//   //         user: {
-//   //           email,
-//   //           firstName,
-//   //           lastName
-//   //         },
-//   //         exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
-//   //       },
-//   //       JWT_SECRET,
-//   //       {
-//   //         algorithm: 'HS256',
-//   //         subject: email
-//   //       }
-//   //     );
-//   //
-//   //     return chai
-//   //       .request(app)
-//   //       .post('/api/auth/refresh')
-//   //       .set('authorization', `Bearer ${token}`)
-//   //       .then(() =>
-//   //         expect.fail(null, null, 'Request should not succeed')
-//   //       )
-//   //       .catch(err => {
-//   //         if (err instanceof chai.AssertionError) {
-//   //           throw err;
-//   //         }
-//   //
-//   //         const res = err.response;
-//   //         expect(res).to.have.status(401);
-//   //       });
-//   //   });
-//   //   it('Should return a valid auth token with a newer expiry date', function () {
-//   //     const token = jwt.sign(
-//   //       {
-//   //         user: {
-//   //           email,
-//   //           firstName,
-//   //           lastName
-//   //         }
-//   //       },
-//   //       JWT_SECRET,
-//   //       {
-//   //         algorithm: 'HS256',
-//   //         subject: username,
-//   //         expiresIn: '7d'
-//   //       }
-//   //     );
-//   //     const decoded = jwt.decode(token);
-//   //
-//   //     return chai
-//   //       .request(app)
-//   //       .post('/api/auth/refresh')
-//   //       .set('authorization', `Bearer ${token}`)
-//   //       .then(res => {
-//   //         expect(res).to.have.status(200);
-//   //         expect(res.body).to.be.an('object');
-//   //         const token = res.body.authToken;
-//   //         expect(token).to.be.a('string');
-//   //         const payload = jwt.verify(token, JWT_SECRET, {
-//   //           algorithm: ['HS256']
-//   //         });
-//   //         expect(payload.user).to.deep.equal({
-//   //           email,
-//   //           firstName,
-//   //           lastName
-//   //         });
-//   //         expect(payload.exp).to.be.at.least(decoded.exp);
-//   //       });
-//   //   });
-//   // });
-// });
+// //   //   it('Should reject requests with incorrect usernames', function () {
+// //   //     return chai
+// //   //       .request(app)
+// //   //       .post('/api/auth/login')
+// //   //       .send({ email: 'wrongemail', password })
+// //   //       .then(() =>
+// //   //         expect.fail(null, null, 'Request should not succeed')
+// //   //       )
+// //   //       .catch(err => {
+// //   //         if (err instanceof chai.AssertionError) {
+// //   //           throw err;
+// //   //         }
+// //   //
+// //   //         const res = err.response;
+// //   //         expect(res).to.have.status(401);
+// //   //       });
+// //   //   });
+// //   //   it('Should reject requests with incorrect passwords', function () {
+// //   //     return chai
+// //   //       .request(app)
+// //   //       .post('/api/auth/login')
+// //   //       .send({ email, password: 'wrongPassword' })
+// //   //       .then(() =>
+// //   //         expect.fail(null, null, 'Request should not succeed')
+// //   //       )
+// //   //       .catch(err => {
+// //   //         if (err instanceof chai.AssertionError) {
+// //   //           throw err;
+// //   //         }
+// //   //
+// //   //         const res = err.response;
+// //   //         expect(res).to.have.status(401);
+// //   //       });
+// //   //   });
+// //   //   it('Should return a valid auth token', function () {
+// //   //     return chai
+// //   //       .request(app)
+// //   //       .post('/api/auth/login')
+// //   //       .send({ email, password })
+// //   //       .then(res => {
+// //   //         expect(res).to.have.status(200);
+// //   //         expect(res.body).to.be.an('object');
+// //   //         const token = res.body.authToken;
+// //   //         expect(token).to.be.a('string');
+// //   //         const payload = jwt.verify(token, JWT_SECRET, {
+// //   //           algorithm: ['HS256']
+// //   //         });
+// //   //         expect(payload.user).to.deep.equal({
+// //   //           email,
+// //   //           firstName,
+// //   //           lastName
+// //   //         });
+// //   //       });
+// //   //   });
+// //   });
+// //
+// //   // describe('/api/auth/refresh', function () {
+// //   //   it('Should reject requests with no credentials', function () {
+// //   //     return chai
+// //   //       .request(app)
+// //   //       .post('/api/auth/refresh')
+// //   //       .then(() =>
+// //   //         expect.fail(null, null, 'Request should not succeed')
+// //   //       )
+// //   //       .catch(err => {
+// //   //         if (err instanceof chai.AssertionError) {
+// //   //           throw err;
+// //   //         }
+// //   //
+// //   //         const res = err.response;
+// //   //         expect(res).to.have.status(401);
+// //   //       });
+// //   //   });
+// //   //   it('Should reject requests with an invalid token', function () {
+// //   //     const token = jwt.sign(
+// //   //       {
+// //   //         email,
+// //   //         firstName,
+// //   //         lastName
+// //   //       },
+// //   //       'wrongSecret',
+// //   //       {
+// //   //         algorithm: 'HS256',
+// //   //         expiresIn: '7d'
+// //   //       }
+// //   //     );
+// //   //
+// //   //     return chai
+// //   //       .request(app)
+// //   //       .post('/api/auth/refresh')
+// //   //       .set('Authorization', `Bearer ${token}`)
+// //   //       .then(() =>
+// //   //         expect.fail(null, null, 'Request should not succeed')
+// //   //       )
+// //   //       .catch(err => {
+// //   //         if (err instanceof chai.AssertionError) {
+// //   //           throw err;
+// //   //         }
+// //   //
+// //   //         const res = err.response;
+// //   //         expect(res).to.have.status(401);
+// //   //       });
+// //   //   });
+// //   //   it('Should reject requests with an expired token', function () {
+// //   //     const token = jwt.sign(
+// //   //       {
+// //   //         user: {
+// //   //           email,
+// //   //           firstName,
+// //   //           lastName
+// //   //         },
+// //   //         exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
+// //   //       },
+// //   //       JWT_SECRET,
+// //   //       {
+// //   //         algorithm: 'HS256',
+// //   //         subject: email
+// //   //       }
+// //   //     );
+// //   //
+// //   //     return chai
+// //   //       .request(app)
+// //   //       .post('/api/auth/refresh')
+// //   //       .set('authorization', `Bearer ${token}`)
+// //   //       .then(() =>
+// //   //         expect.fail(null, null, 'Request should not succeed')
+// //   //       )
+// //   //       .catch(err => {
+// //   //         if (err instanceof chai.AssertionError) {
+// //   //           throw err;
+// //   //         }
+// //   //
+// //   //         const res = err.response;
+// //   //         expect(res).to.have.status(401);
+// //   //       });
+// //   //   });
+// //   //   it('Should return a valid auth token with a newer expiry date', function () {
+// //   //     const token = jwt.sign(
+// //   //       {
+// //   //         user: {
+// //   //           email,
+// //   //           firstName,
+// //   //           lastName
+// //   //         }
+// //   //       },
+// //   //       JWT_SECRET,
+// //   //       {
+// //   //         algorithm: 'HS256',
+// //   //         subject: username,
+// //   //         expiresIn: '7d'
+// //   //       }
+// //   //     );
+// //   //     const decoded = jwt.decode(token);
+// //   //
+// //   //     return chai
+// //   //       .request(app)
+// //   //       .post('/api/auth/refresh')
+// //   //       .set('authorization', `Bearer ${token}`)
+// //   //       .then(res => {
+// //   //         expect(res).to.have.status(200);
+// //   //         expect(res.body).to.be.an('object');
+// //   //         const token = res.body.authToken;
+// //   //         expect(token).to.be.a('string');
+// //   //         const payload = jwt.verify(token, JWT_SECRET, {
+// //   //           algorithm: ['HS256']
+// //   //         });
+// //   //         expect(payload.user).to.deep.equal({
+// //   //           email,
+// //   //           firstName,
+// //   //           lastName
+// //   //         });
+// //   //         expect(payload.exp).to.be.at.least(decoded.exp);
+// //   //       });
+// //   //   });
+// //   // });
+// })
--- test/test-protected.js
+++ test/test-protected.js
@@ -20,11 +20,11 @@ describe('Protected endpoint', function() {
   const lastName = 'User';
 
   before(function() {
-    return runServer();
+    return runServer(TEST_DATABASE_URL);
   });
 
   after(function() {
-    return closeServer();
+    return closeServer(TEST_DATABASE_URL);
   });
 
   beforeEach(function() {
@@ -44,111 +44,112 @@ describe('Protected endpoint', function() {
 
   describe('/api/protected', function() {
     it('Should reject requests with no credentials', function() {
+      console.log(User);
       return chai
         .request(app)
         .get('/api/protected')
-  //       .then(() =>
-  //         expect.fail(null, null, 'Request should not succeed')
-  //       )
-  //       .catch(err => {
-  //         if (err instanceof chai.AssertionError) {
-  //           throw err;
-  //         }
-  //
-  //         const res = err.response;
-  //         expect(res).to.have.status(401);
-  //       });
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
     });
-  //
-  //   it('Should reject requests with an invalid token', function() {
-  //     const token = jwt.sign(
-  //       {
-  //         email,
-  //         firstName,
-  //         lastName
-  //       },
-  //       'wrongSecret',
-  //       {
-  //         algorithm: 'HS256',
-  //         expiresIn: '7d'
-  //       }
-  //     );
-  //
-  //     return chai
-  //       .request(app)
-  //       .get('/api/protected')
-  //       .set('Authorization', `Bearer ${token}`)
-  //       .then(() =>
-  //         expect.fail(null, null, 'Request should not succeed')
-  //       )
-  //       .catch(err => {
-  //         if (err instanceof chai.AssertionError) {
-  //           throw err;
-  //         }
-  //
-  //         const res = err.response;
-  //         expect(res).to.have.status(401);
-  //       });
+
+    it('Should reject requests with an invalid token', function() {
+      const token = jwt.sign(
+        {
+          email,
+          firstName,
+          lastName
+        },
+        'wrongSecret',
+        {
+          algorithm: 'HS256',
+          expiresIn: '7d'
+        }
+      );
+
+      return chai
+        .request(app)
+        .get('/api/protected')
+        .set('Authorization', `Bearer ${token}`)
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
     });
-  //   it('Should reject requests with an expired token', function() {
-  //     const token = jwt.sign(
-  //       {
-  //         user: {
-  //           email,
-  //           firstName,
-  //           lastName
-  //         },
-  //         exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
-  //       },
-  //       JWT_SECRET,
-  //       {
-  //         algorithm: 'HS256',
-  //         subject: email
-  //       }
-  //     );
-  //
-  //     return chai
-  //       .request(app)
-  //       .get('/api/protected')
-  //       .set('authorization', `Bearer ${token}`)
-  //       .then(() =>
-  //         expect.fail(null, null, 'Request should not succeed')
-  //       )
-  //       .catch(err => {
-  //         if (err instanceof chai.AssertionError) {
-  //           throw err;
-  //         }
-  //
-  //         const res = err.response;
-  //         expect(res).to.have.status(401);
-  //       });
-  //   });
-  //   it('Should send protected data', function() {
-  //     const token = jwt.sign(
-  //       {
-  //         user: {
-  //           email,
-  //           firstName,
-  //           lastName
-  //         }
-  //       },
-  //       JWT_SECRET,
-  //       {
-  //         algorithm: 'HS256',
-  //         subject: email,
-  //         expiresIn: '7d'
-  //       }
-  //     );
-  //
-  //     return chai
-  //       .request(app)
-  //       .get('/api/protected')
-  //       .set('authorization', `Bearer ${token}`)
-  //       .then(res => {
-  //         expect(res).to.have.status(200);
-  //         expect(res.body).to.be.an('object');
-  //         expect(res.body.data).to.equal('rosebud');
-  //       });
-  //   });
-  // });
+    it('Should reject requests with an expired token', function() {
+      const token = jwt.sign(
+        {
+          user: {
+            email,
+            firstName,
+            lastName
+          },
+          exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
+        },
+        JWT_SECRET,
+        {
+          algorithm: 'HS256',
+          subject: email
+        }
+      );
+
+      return chai
+        .request(app)
+        .get('/api/protected')
+        .set('authorization', `Bearer ${token}`)
+        .then(() =>
+          expect.fail(null, null, 'Request should not succeed')
+        )
+        .catch(err => {
+          if (err instanceof chai.AssertionError) {
+            throw err;
+          }
+
+          const res = err.response;
+          expect(res).to.have.status(401);
+        });
+    });
+    it('Should send protected data', function() {
+      const token = jwt.sign(
+        {
+          user: {
+            email,
+            firstName,
+            lastName
+          }
+        },
+        JWT_SECRET,
+        {
+          algorithm: 'HS256',
+          subject: email,
+          expiresIn: '7d'
+        }
+      );
+
+      return chai
+        .request(app)
+        .get('/api/protected')
+        .set('authorization', `Bearer ${token}`)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.equal('rosebud');
+        });
+    });
+  });
 });
