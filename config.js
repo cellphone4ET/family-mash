4a9c4b8803e2e02ffb524332db079da11a70f4d2
Erica Johnson <ericacjohnson@gmail.com>
2018-11-05 12:57:51 -0800

    export edits

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
-exports.DATABASE_URL =  process.env.DATABASE_URL || 'mongodb://localhost/family-mash';
-exports.TEST_DATABASE_URL =  process.env.TEST_DATABASE_URL || 'mongodb://localhost/family-mash-test';
+exports.DATABASE_URL =
+  process.env.DATABASE_URL || "mongodb://localhost/family-mash";
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/family-mash-test";
 exports.PORT = process.env.PORT || 8080;
 
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
